26bdfa715addd06c40b76a2d26d00a3e3b7b1f4e
Jitendra Giriya <jitendragiriya@gmail.com>
2022-12-08 11:00:07 +0530

    base url changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://myshop-chat-backend1-com.onrender.com/";
+export const BASE_URL = "https://myshop-chat-backend1-com.onrender.com";
 
 export const TOKEN = "log_data"
 
-export const ENDPOINT = "https://myshop-chat-backend1-com.onrender.com/"
+export const ENDPOINT = "https://myshop-chat-backend1-com.onrender.com"
