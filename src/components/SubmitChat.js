2ffedd0cb05c2b10ee9fd025f2aacdc16de6bb6c
jitendra giriya <jitendragiriya@gmail.com>
2023-09-05 13:48:45 +0530

    update google auth

--- src/components/SubmitChat.js
+++ src/components/SubmitChat.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import Button from "@mui/material/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { USER_MESSAGES_SUCCESS } from "../constants";
 
 const SubmitChat = ({ recepient, socket }) => {
   const [message, setMessage] = useState("");
   const { user } = useSelector((state) => state.User);
+  const { messages } = useSelector((state) => state.Messages);
+  const dispatch = useDispatch();
 
   // Function to send a private message
   const sendPrivateMessage = () => {
-    socket.emit("privateMessage", {
+    const chat = {
       senderId: user?._id,
       receiverId: recepient?._id,
       message,
+      time: new Date(),
+    };
+    socket.emit("privateMessage", chat);
+    dispatch({
+      type: USER_MESSAGES_SUCCESS,
+      payload: messages?.length ? [...messages, chat] : [chat],
     });
     setMessage("");
   };
